b8756615c0cc584df4075e701a5ba708b80889dc
prameela <prameelarpujari>
2024-02-22 15:15:44 +0530

    Added handleClick. Data sharing between Square and Board is working properly.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>It's React</h1>
+    <h2>First React Program</h2>
+    </div>
+  );
+}
+export default App;
